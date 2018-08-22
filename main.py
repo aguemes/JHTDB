5187d763af5a829f93e341787b237ed8664d008f
aguemes <alexgj93@gmail.com>
2018-08-22 19:20:59 +0200

    First version of the code where JHTDB runs

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ import random
 import pyJHTDB
 import matplotlib.pyplot as plt
 
+# load shared library
+lTDB = pyJHTDB.libJHTDB()
+#initialize webservices
+lTDB.initialize()
+
+t1 = np.linspace(0, 2*3.14, 64)
+t2 = np.linspace(0, 1, 64)
+x = np.zeros((t1.shape[0], t2.shape[0], 3), np.float32)
+x[:, :, 0] = t1[np.newaxis, :]
+x[:, :, 1] = t2[:, np.newaxis]
+x[:, :, 2] = .0
+
+T = pyJHTDB.dbinfo.channel['time'][-1]
+time = np.random.random()*T
+u = lTDB.getData(time, x, data_set = 'channel', sinterp = 4, getFunction='getVelocity')
+np.savez('data', x, u, time)
+e = np.sum(u**2, axis = 2)
+fig = plt.figure(figsize = (10, 5))
+a = fig.add_subplot(121)
+a.imshow(u[:,:,0], extent = [t1[0], t1[-1] - t1[0], t2[0], t2[-1] - t2[0]], interpolation = 'none', aspect = 'equal')
+plt.show()
+lTDB.finalize()
